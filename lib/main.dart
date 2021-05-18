386c1da5b13d6a58e31f7bcb792cb5440c8a2696
bilguun1001 <bilguunkhasherdene@gmail.com>
2021-05-19 00:07:37 +0800

    HW

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+
 import 'package:flutter/material.dart';
-import 'package:mongol/mongol.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -8,27 +9,67 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(home: HomePage());
   }
 }
 
 class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,) {
+
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 200,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: MongolText(
-              'ᠨᠠᠮᠠᠢ ᠪᠢᠯᠢᠭᠦᠨ ᠭᠡᠳᠡᠭ᠃ ᠪᠢ 20 ᠨᠠᠰᠤᠲᠠᠢ᠃ ᠪᠢ ᠡᠮ-ᠠᠢ-ᠶᠦ ᠳᠡᠭᠡᠳᠦ ᠰᠤᠷᠭᠠᠭᠤᠯᠢ ᠳᠤ ᠰᠤᠷᠠᠳᠠᠭ᠃ ᠮᠠᠨ ᠤ ᠠᠮᠠ ᠪᠦᠯᠢ 5᠃ ᠪᠢ ᠳᠣᠣᠷ᠎ᠠ ᠪᠠᠨ 2 ᠳᠡᠭᠦᠦ ᠲᠡᠢ᠃ ᠲᠣᠮ ᠨᠢ ᠡᠮᠡᠭᠲᠡᠢ ᠠᠩᠬᠢᠯᠤᠭᠤᠨ ᠭᠡᠳᠡᠭ᠃ ᠪᠠᠭ᠎ᠠ ᠶᠢ ᠨᠢ ᠴᠢᠩ ᠦᠨ ᠭᠡᠳᠡᠭ᠃',
-              style: TextStyle(fontFamily: 'MongolianScript', fontSize: 30),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.blueAccent,
+              child: OrientationBuilder(
+                builder: (context, orientation,) => Center(
+                  child: Message(deviceOrientation: orientation),
+                ),
+              ),
             ),
           ),
-        ),
+          Expanded(
+            flex: 3,
+            child: OrientationBuilder(
+              builder: (context, orientation,) => Container(
+                color: Colors.white,
+                child: Center(
+                  child: Message(deviceOrientation: orientation),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Message extends StatelessWidget {
+  const Message({
+    Key key,
+    @required this.deviceOrientation,
+  }) : super(key: key);
+
+  final Orientation deviceOrientation;
+
+  @override
+  Widget build(BuildContext context) {
+    String message;
+
+    if ( deviceOrientation == Orientation.portrait ){
+      message = "hello";
+    }
+    else{
+      message = "Hello. It is nice to meet you.";
+    }
+    return Text(
+      message,
+      style: TextStyle(color: Colors.black, fontSize: 18),
     );
   }
 }
