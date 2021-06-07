ce6647cf2a7867510923125db9dcc1ba2699679e
bilguun1001 <bilguunkhasherdene@gmail.com>
2021-06-07 16:13:02 +0800

    'mycode'

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,29 @@
-
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'dart:html' as html;
+import 'package:camera/camera.dart';
+import 'home.dart';
 
-void main() {
-  runApp(MyApp());
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.code\nError Message: $e.message');
+  }
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Skima.dev',
+      title: 'tflite real-time detection',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
+        brightness: Brightness.dark,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              title: Text('Sports app'),
-              onTap: () => html.window.open(
-                'https://play.google.com/store/apps/details?id=com.bilguun.skima&fbclid=IwAR2NIjfYlVqZq0YJG8jmW_gMYs2tx33fsdMz9fyWyoPtnJ1IBa0DGrJSWzY',
-                'Sports app',
-              ),
-            )
-          ],
-        ),
-      ),
-      body: ListView(
-        children: [
-          SizedBox(height: 100),
-          SizedBox(
-            width: 200,
-            height: 200,
-            child: Image.asset('assets/bilguun.jpg'),
-          ),
-          SizedBox(height: 50),
-          Center(child: Text('Skima', style: TextStyle(fontSize: 40))),
-          Center(child: SelectableText('https://bilguun1001.github.io/')),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              GitHubButton(),
-              SizedBox(width: 20),
-              TwitterButton(),
-              SizedBox(width: 20),
-              MediumButton(),
-              SizedBox(width: 20),
-              StackOverflowButton(),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class GitHubButton extends StatelessWidget {
-  const GitHubButton({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Image.asset('assets/github.png'),
-      iconSize: 30,
-      onPressed: () =>
-          html.window.open('https://github.com/bilguun1001/Skima', 'Skima'),
-    );
-  }
-}
-
-class TwitterButton extends StatelessWidget {
-  const TwitterButton({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Image.asset('assets/facebook.png'),
-      iconSize: 30,
-      onPressed: () =>
-          html.window.open('https://www.facebook.com/bilguun.khasherdene/', 'Х.Билгүүн '),
-    );
-  }
-}
-
-class MediumButton extends StatelessWidget {
-  const MediumButton({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Image.asset('assets/instagram.png'),
-      iconSize: 30,
-      onPressed: () => html.window.open(
-          'https://www.instagram.com/_bilguuunn_/?hl=en',
-          '_bilguuunn_'),
-    );
-  }
-}
-
-class StackOverflowButton extends StatelessWidget {
-  const StackOverflowButton({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Image.asset('assets/twitter.png'),
-      iconSize: 30,
-      onPressed: () => html.window
-          .open('https://twitter.com/bilguun91903312', 'bilguun'),
+      home: HomePage(cameras),
     );
   }
 }
